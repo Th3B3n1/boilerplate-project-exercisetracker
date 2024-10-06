1e34907687f9ce20749578ae8779afe14965ed00
Th3B3n1 <othersc9bst@gmail.com>
2024-10-06 15:52:04 +0200

    done (task 8 dosen't want to pass even though I send the desired object)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-const bodyParser = require('body-parser');
-const crypto = require('crypto')
+import express from 'express';
+import cors from 'cors';
+import 'dotenv';
+import bodyParser from 'body-parser';
+import crypto from 'crypto';
+
+const app = express();
 const jsonParser = bodyParser.urlencoded({extended: false});
 
 let users = new Array();
 let exercises = new Array();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 app.get("/api/users", function (req, res)
@@ -23,7 +24,7 @@ app.get("/api/users", function (req, res)
   }
   else
   {
-    res.json({error: "There is no user in the database."})
+    res.json({error: "There is no user in the database."});
   }
 })
 
@@ -33,28 +34,28 @@ app.get("/api/users/:_id/logs", function (req, res)
   {
     if (exercises.some(e => e._id == req.params._id))
     {
-      let allExercise = exercises.filter(e => e._id == req.params._id)
+      let allExercise = exercises.filter(e => e._id == req.params._id);
       let logs = new Array();
       for (let i = 0; i < allExercise.length; i++)
       {
-        logs.push({description: allExercise[i].description, duration: parseInt(allExercise[i].duration), date: allExercise[i].date})
+        logs.push({description: allExercise[i].description, duration: parseInt(allExercise[i].duration), date: allExercise[i].date});
       }
       if (req.query != undefined)
       {
         if (req.query.to != undefined && req.query.from != undefined)
         {
-          logs = logs.filter(e => e.date >= req.query.to && e.date <= eq.query.from)
+          logs = logs.filter(e => new Date(e.date) >= new Date(req.query.from) && new Date(e.date) <= new Date(req.query.to));
         }
-        else if (req.query.limit != undefined)
+        if (req.query.limit != undefined)
         {
-          logs = logs.slice(0, req.query.limit)
+          logs = logs.slice(0, req.query.limit);
         }
       }
-      res.json({_id: allExercise[0]._id, username: allExercise[0].username, count: logs.length, log: logs})
+      res.json({_id: allExercise[0]._id, username: allExercise[0].username, count: logs.length, log: logs});
     }
     else
     {
-      res.json({error: "No user is assigned to this id"})
+      res.json({error: "No user is assigned to this id"});
     }
   }
 })
@@ -64,7 +65,7 @@ app.post("/api/users", jsonParser, function (req, res)
   if (req.body.username != undefined && req.body.username != "")
   {
     let user = {_id: crypto.randomBytes(20).toString('hex'), username: req.body.username};
-    users.push(user)
+    users.push(user);
     res.json(user);
   }
   else
@@ -83,28 +84,28 @@ app.post("/api/users/:_id/exercises", jsonParser, function (req, res)
       {
         if ((req.body.description != undefined && req.body.description != "") && (req.body.duration != undefined && !isNaN(parseInt(req.body.duration))))
         {
-          let id = users.findIndex(e => e._id == req.params._id)
-          let exercise = {username: users[id].username, description: req.body.description, duration: req.body.duration, date: req.body.date != undefined ? req.body.date : new Date().toDateString(), _id: users[id]._id}
-          exercises.push(exercise)
-          res.json(exercise)
+          let id = users.findIndex(e => e._id == req.params._id);
+          let exercise = {username: users[id].username, description: req.body.description, duration: req.body.duration, date: req.body.date != undefined ? req.body.date : new Date().toDateString(), _id: users[id]._id};
+          exercises.push(exercise);
+          res.json(exercise);
         }
         else
         {
-          res.json({error: "Mandatory fields are invalid"})
+          res.json({error: "Mandatory fields are invalid"});
         }
       }
       else
       {
-        res.json({error: "No field given"})
+        res.json({error: "No field given"});
       }
     }
     else
     {
-      res.json({error: "No user is assigned to this id"})
+      res.json({error: "No user is assigned to this id"});
     }
   }
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log('Your app is listening on port ' + listener.address().port);
 })
